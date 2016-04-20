4d504a5212c946df5aac8b33c289d5d69c0c6bbc
athanasios <bozios_20@hotmail.gr>
2016-04-20 22:21:18 +0200

    david tests stuff

--- 1DV508/src/users/User.java
+++ 1DV508/src/users/User.java
@@ -4,7 +4,8 @@ public class User{
 	
 	private String username;
 	private String password;
-	private int id;	
+	private int id;
+	private int DavidTestsStuff;
 	
 	public int getId() {
 		return id;
