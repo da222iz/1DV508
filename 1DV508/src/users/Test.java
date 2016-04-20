9dcd4a07f57d3d96b25e63820cc968f98cafb752
athanasios <bozios_20@hotmail.gr>
2016-04-20 10:55:21 +0200

    test class

---
+++ 1DV508/src/users/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package users;
+
+/**
+ * @author athanasios
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
